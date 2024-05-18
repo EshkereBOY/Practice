523094ec4b0473fe41306b819cb45972c1dc3d29
EshkereBOY <mutin_artur@mail.ru>
2024-05-18 23:23:59 +1000

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
+import { useForm, SubmitHandler } from "react-hook-form"
+import './App.css'
+
+export const App = () => {
+
+  return (
+    <>
+      <h1>front haha</h1>
+      <button>front haha</button>
+    </>
+  )
+}
+
